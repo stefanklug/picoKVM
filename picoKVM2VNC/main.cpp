d51259cad6dfd97cfcad5cdd3d606c44c3a1d882
Stefan Klug <klug.stefan@gmx.de>
2021-11-04 23:26:58 +0100

    Cleanup
    This started from a v4l2 sample and I missed to correct the license in first place.

--- picoKVM2VNC/main.cpp
+++ picoKVM2VNC/main.cpp
@@ -1,14 +1,18 @@
-/* V4L2 video picture grabber
-    Copyright (C) 2009 Mauro Carvalho Chehab <mchehab@infradead.org>
+/*  picoKVM2VNC
+    Copyright (C) 2021 Stefan Klug <info@stefanklug.com>
 
-    This program is free software; you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation version 2 of the License.
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
 */
 
 #include <stdio.h>
