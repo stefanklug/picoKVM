42b283fe896f0b1f4bb06fc363ccded31c97bae0
Steven Scholz <steven.scholz@baslerweb.com>
2022-07-05 22:34:06 +0200

    Add nativeScanToHID table for Windows

--- picoKVMViewer/ExtendedVideoWidget.cpp
+++ picoKVMViewer/ExtendedVideoWidget.cpp
@@ -9,6 +9,7 @@
 
 #define MAX_KEYS 6
 
+#ifdef Q_OS_LINUX
 std::map<uint32_t, int> nativeScanToHID = {
     {0x09, KEY_ESC},
 
@@ -124,7 +125,148 @@ std::map<uint32_t, int> nativeScanToHID = {
     {0x41, KEY_SPACE}
 };
 
+uint8_t nativeScanCodeToHidModifier(uint32_t scanCode) {
+    switch(scanCode) {
+        case 0x25: return KEY_MOD_LCTRL;
+        case 0x32: return KEY_MOD_LSHIFT;
+        case 0x40: return KEY_MOD_LALT;
+        case 0x85: return KEY_MOD_LMETA;
+        case 0x69: return KEY_MOD_RCTRL;
+        case 0x3e: return KEY_MOD_RSHIFT;
+        case 0x6c: return KEY_MOD_RALT;
+        case 0x87: return KEY_MOD_RMETA;
+        default: return 0;
+    }
+}
+
 #define PICOKVM_RELEASE_KEY 0x69    // right ctrl
+#endif // Q_OS_LINUX
+
+#ifdef Q_OS_WINDOWS
+// https://en.wikipedia.org/wiki/Keyboard_layout#/media/File:ISO_keyboard_(105)_QWERTY_UK.svg
+std::map<uint32_t, int> nativeScanToHID = {
+    {0x1, KEY_ESC},  //  QKeyEvent(KeyPress, KEY_Escape, text="\u001B")
+    {0x3b, KEY_F1},  //  QKeyEvent(KeyPress, KEY_F1)
+    {0x3c, KEY_F2},  //  QKeyEvent(KeyPress, KEY_F2)
+    {0x3d, KEY_F3},  //  QKeyEvent(KeyPress, KEY_F3)
+    {0x3e, KEY_F4},  //  QKeyEvent(KeyPress, KEY_F4)
+    {0x3f, KEY_F5},  //  QKeyEvent(KeyPress, KEY_F5)
+    {0x40, KEY_F6},  //  QKeyEvent(KeyPress, KEY_F6)
+    {0x41, KEY_F7},  //  QKeyEvent(KeyPress, KEY_F7)
+    {0x42, KEY_F8},  //  QKeyEvent(KeyPress, KEY_F8)
+    {0x43, KEY_F9},  //  QKeyEvent(KeyPress, KEY_F9)
+    {0x44, KEY_F10},  //  QKeyEvent(KeyPress, KEY_F10)
+    {0x57, KEY_F11},  //  QKeyEvent(KeyPress, KEY_F11)
+    {0x58, KEY_F12},  //  QKeyEvent(KeyPress, KEY_F12)
+    //{0x100, },  //  QKeyEvent(KeyPress, KEY_VolumeMute)
+    //{0x100, },  //  QKeyEvent(KeyPress, KEY_VolumeDown)
+    //{0x100, },  //  QKeyEvent(KeyPress, KEY_VolumeUp)
+    {0x2, KEY_1},  //  QKeyEvent(KeyPress, KEY_1, text="1")
+    {0x3, KEY_2},  //  QKeyEvent(KeyPress, KEY_2, text="2")
+    {0x4, KEY_3},  //  QKeyEvent(KeyPress, KEY_3, text="3")
+    {0x5, KEY_4},  //  QKeyEvent(KeyPress, KEY_4, text="4")
+    {0x6, KEY_5},  //  QKeyEvent(KeyPress, KEY_5, text="5")
+    {0x7, KEY_6},  //  QKeyEvent(KeyPress, KEY_6, text="6")
+    {0x8, KEY_7},  //  QKeyEvent(KeyPress, KEY_7, text="7")
+    {0x9, KEY_8},  //  QKeyEvent(KeyPress, KEY_8, text="8")
+    {0xa, KEY_9},  //  QKeyEvent(KeyPress, KEY_9, text="9")
+    {0xb, KEY_0},  //  QKeyEvent(KeyPress, KEY_0, text="0")
+    {0xc, KEY_MINUS},  //  QKeyEvent(KeyPress, KEY_ssharp, text="▀")
+    {0xe, KEY_BACKSPACE},  //  QKeyEvent(KeyPress, KEY_Backspace, text="\b")
+    {0xf, KEY_TAB},  //  QKeyEvent(KeyPress, KEY_Tab, text="\t")
+    {0x10, KEY_Q},  //  QKeyEvent(KeyPress, KEY_Q, text="q")
+    {0x11, KEY_W},  //  QKeyEvent(KeyPress, KEY_W, text="w")
+    {0x12, KEY_E},  //  QKeyEvent(KeyPress, KEY_E, text="e")
+    {0x13, KEY_R},  //  QKeyEvent(KeyPress, KEY_R, text="r")
+    {0x14, KEY_T},  //  QKeyEvent(KeyPress, KEY_T, text="t")
+    {0x15, KEY_Z},  //  QKeyEvent(KeyPress, KEY_Z, text="z")
+    {0x16, KEY_U},  //  QKeyEvent(KeyPress, KEY_U, text="u")
+    {0x17, KEY_I},  //  QKeyEvent(KeyPress, KEY_I, text="i")
+    {0x18, KEY_O},  //  QKeyEvent(KeyPress, KEY_O, text="o")
+    {0x19, KEY_P},  //  QKeyEvent(KeyPress, KEY_P, text="p")
+    {0x1a, KEY_LEFTBRACE},  //  QKeyEvent(KeyPress, KEY_Udiaeresis, text="³")
+    {0x1b, KEY_RIGHTBRACE},  //  QKeyEvent(KeyPress, KEY_Plus, text="+")
+    {0x1c, KEY_ENTER},  //  QKeyEvent(KeyPress, KEY_Return, text="\r")
+    {0x3a, KEY_CAPSLOCK},  //  QKeyEvent(KeyPress, KEY_CapsLock)
+    {0x1e, KEY_A},  //  QKeyEvent(KeyPress, KEY_A, text="a")
+    {0x1f, KEY_S},  //  QKeyEvent(KeyPress, KEY_S, text="s")
+    {0x20, KEY_D},  //  QKeyEvent(KeyPress, KEY_D, text="d")
+    {0x21, KEY_F},  //  QKeyEvent(KeyPress, KEY_F, text="f")
+    {0x22, KEY_G},  //  QKeyEvent(KeyPress, KEY_G, text="g")
+    {0x23, KEY_H},  //  QKeyEvent(KeyPress, KEY_H, text="h")
+    {0x24, KEY_J},  //  QKeyEvent(KeyPress, KEY_J, text="j")
+    {0x25, KEY_K},  //  QKeyEvent(KeyPress, KEY_K, text="k")
+    {0x26, KEY_L},  //  QKeyEvent(KeyPress, KEY_L, text="l")
+    {0x27, KEY_SEMICOLON},  //  QKeyEvent(KeyPress, KEY_Odiaeresis, text="÷")
+    {0x28, KEY_APOSTROPHE},  //  QKeyEvent(KeyPress, KEY_Adiaeresis, text="õ")
+    {0x2b, KEY_HASHTILDE},  //  QKeyEvent(KeyPress, KEY_NumberSign, text="#")
+    //{0x2a, },  //  QKeyEvent(KeyPress, KEY_Shift, ShiftModifier)
+    {0x56, KEY_102ND},  //  QKeyEvent(KeyPress, KEY_Less, text="<")
+    {0x2c, KEY_Y},  //  QKeyEvent(KeyPress, KEY_Y, text="y")
+    {0x2d, KEY_X},  //  QKeyEvent(KeyPress, KEY_X, text="x")
+    {0x2e, KEY_C},  //  QKeyEvent(KeyPress, KEY_C, text="c")
+    {0x2f, KEY_V},  //  QKeyEvent(KeyPress, KEY_V, text="v")
+    {0x30, KEY_B},  //  QKeyEvent(KeyPress, KEY_B, text="b")
+    {0x31, KEY_N},  //  QKeyEvent(KeyPress, KEY_N, text="n")
+    {0x32, KEY_M},  //  QKeyEvent(KeyPress, KEY_M, text="m")
+    {0x33, KEY_COMMA},  //  QKeyEvent(KeyPress, KEY_Comma, text=",")
+    {0x34, KEY_DOT},  //  QKeyEvent(KeyPress, KEY_Period, text=".")
+    {0x35, KEY_SLASH},  //  QKeyEvent(KeyPress, KEY_Minus, text="-")
+    //{0x36, },  //  QKeyEvent(KeyPress, KEY_Shift, ShiftModifier)
+    //{0x1d, },  //  QKeyEvent(KeyPress, KEY_Control, ControlModifier)
+    //{0x15b, KEY_LEFTMETA},  //  QKeyEvent(KeyPress, KEY_Meta)
+    //{0x38, KEY_LEFTALT},  //  QKeyEvent(KeyPress, KEY_Alt, AltModifier)
+    {0x39, KEY_SPACE},  //  QKeyEvent(KeyPress, KEY_Space, text=" ")
+    //{0x138, },  //  QKeyEvent(KeyPress, KEY_Alt, ControlModifier|AltModifier)
+    //{0x15d, },  //  QKeyEvent(KeyPress, KEY_Menu)
+    //{0x11d, },  //  QKeyEvent(KeyPress, KEY_Control, ControlModifier)
+    {0x152, KEY_INSERT},  //  QKeyEvent(KeyPress, KEY_Insert)
+    {0x147, KEY_HOME},  //  QKeyEvent(KeyPress, KEY_Home)
+    {0x149, KEY_PAGEUP},  //  QKeyEvent(KeyPress, KEY_PageUp)
+    {0x153, KEY_DELETE},  //  QKeyEvent(KeyPress, KEY_Delete, text="\u007F")
+    {0x14f, KEY_END},  //  QKeyEvent(KeyPress, KEY_End)
+    {0x151, KEY_PAGEDOWN},  //  QKeyEvent(KeyPress, KEY_PageDown)
+    {0x14b, KEY_LEFT},  //  QKeyEvent(KeyPress, KEY_Left)
+    {0x150, KEY_DOWN},  //  QKeyEvent(KeyPress, KEY_Down)
+    {0x14d, KEY_RIGHT},  //  QKeyEvent(KeyPress, KEY_Right)
+    {0x148, KEY_UP},  //  QKeyEvent(KeyPress, KEY_Up)
+    {0x29, KEY_GRAVE},  //  QKeyEvent(KeyPress, KEY_AsciiCircum, text="^")
+    {0xd, KEY_EQUAL},  //  QKeyEvent(KeyPress, KEY_acute, text="┤")
+    //{0x145, },  //  QKeyEvent(KeyPress, KEY_NumLock, KeypadModifier)
+    //{0x135, },  //  QKeyEvent(KeyPress, KEY_Slash, KeypadModifier, text="/")
+    //{0x37, },  //  QKeyEvent(KeyPress, KEY_Asterisk, KeypadModifier, text="*")
+    //{0x4a, },  //  QKeyEvent(KeyPress, KEY_Minus, KeypadModifier, text="-")
+    //{0x47, },  //  QKeyEvent(KeyPress, KEY_Home, KeypadModifier)
+    //{0x48, },  //  QKeyEvent(KeyPress, KEY_Up, KeypadModifier)
+    //{0x49, },  //  QKeyEvent(KeyPress, KEY_PageUp, KeypadModifier)
+    //{0x4e, },  //  QKeyEvent(KeyPress, KEY_Plus, KeypadModifier, text="+")
+    //{0x4b, },  //  QKeyEvent(KeyPress, KEY_Left, KeypadModifier)
+    //{0x4c, },  //  QKeyEvent(KeyPress, KEY_Clear, KeypadModifier)
+    //{0x4d, },  //  QKeyEvent(KeyPress, KEY_Right, KeypadModifier)
+    //{0x4f, },  //  QKeyEvent(KeyPress, KEY_End, KeypadModifier)
+    //{0x50, },  //  QKeyEvent(KeyPress, KEY_Down, KeypadModifier)
+    //{0x51, },  //  QKeyEvent(KeyPress, KEY_PageDown, KeypadModifier)
+    //{0x52, },  //  QKeyEvent(KeyPress, KEY_Insert, KeypadModifier)
+    //{0x53, },  //  QKeyEvent(KeyPress, KEY_Delete, KeypadModifier, text="\u007F")
+    //{0x11c, },  //  QKeyEvent(KeyPress, KEY_Enter, KeypadModifier, text="\r")
+};
+
+uint8_t nativeScanCodeToHidModifier(uint32_t scanCode) {
+    switch(scanCode) {
+        case 0x1D: return KEY_MOD_LCTRL;
+        case 0x11D: return KEY_MOD_RCTRL;
+        case 0x2A: return KEY_MOD_LSHIFT;
+        case 0x36: return KEY_MOD_RSHIFT;
+        case 0x38: return KEY_MOD_LALT;
+        case 0x138: return KEY_MOD_RALT;
+        case 0x15B: return KEY_MOD_LMETA;
+        case 0x15D: return KEY_MOD_RMETA;
+        default: return 0;
+    }
+}
+#define PICOKVM_RELEASE_KEY 0x11D   // right ctrl
+#endif // Q_OS_WINDOWS
+
 ExtendedVideoWidget::ExtendedVideoWidget(QWidget *parent):QVideoWidget(parent)
 {
     setFocusPolicy(Qt::ClickFocus);
@@ -153,21 +295,6 @@ void ExtendedVideoWidget::setSerialPort(const QString &name)
     mPort.setPortName(name);
     mPort.open(QIODevice::ReadWrite);
 }
-
-uint8_t nativeScanCodeToHidModifier(uint32_t scanCode) {
-    switch(scanCode) {
-        case 0x25: return KEY_MOD_LCTRL;
-        case 0x32: return KEY_MOD_LSHIFT;
-        case 0x40: return KEY_MOD_LALT;
-        case 0x85: return KEY_MOD_LMETA;
-        case 0x69: return KEY_MOD_RCTRL;
-        case 0x3e: return KEY_MOD_RSHIFT;
-        case 0x6c: return KEY_MOD_RALT;
-        case 0x87: return KEY_MOD_RMETA;
-        default: return 0;
-    }
-}
-
 
 void ExtendedVideoWidget::keyPressEvent(QKeyEvent *event)
 {
